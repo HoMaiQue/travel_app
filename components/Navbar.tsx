b347a0f744430170439cae7be2eca41dde4da881
HoMaiQue <homaique.it@gmail.com>
2023-10-17 21:53:07 +0700

    init project

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="flexBetween max-container padding-container relative z-30 py-5">
+            <Link href="/">
+                <Image
+                    src="/hilink-logo.svg"
+                    alt="logo"
+                    width={74}
+                    height={29}
+                />
+            </Link>
+        </nav>
+    );
+}
