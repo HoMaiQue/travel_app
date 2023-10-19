e13d465a203021402cccdf40013eff42826b16f6
HoMaiQue <homaique.it@gmail.com>
2023-10-19 22:19:53 +0700

    camp section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -38,16 +38,58 @@ export default function Hero() {
                     </div>
                     <p className="bold-16 lg:bold-20 text-blue-70 ">
                         198k
-                        <span className="regular-16 lg:regular-20 ml-1 underline">Excellent Reviews</span>
+                        <span className="regular-16 lg:regular-20 ml-1 underline">
+                            Excellent Reviews
+                        </span>
                     </p>
                 </div>
                 <div className="flex w-full gap-3 sm:flex-row flex-col">
-                  <Button type="button" title="Download App" variant="btn_green" />
-                  <Button type="button" title="How we work?" icon="/play.svg" variant="btn_white_text" />
+                    <Button
+                        type="button"
+                        title="Download App"
+                        variant="btn_green"
+                    />
+                    <Button
+                        type="button"
+                        title="How we work?"
+                        icon="/play.svg"
+                        variant="btn_white_text"
+                    />
                 </div>
             </div>
 
-            <div className=""></div>
+            <div className="relative flex flex-1 items-start ">
+                <div className="relative z-20 w-[268px] flex flex-col gap-8 rounded-3xl bg-green-90 py-8 px-7">
+                    <div className="flex flex-col">
+                        <div className="flexBetween">
+                            <p className="regular-16 text-gray-20">Location</p>
+                            <Image
+                                src="/close.svg"
+                                alt="close"
+                                width={24}
+                                height={24}
+                            />
+                        </div>
+                        <p className="bold-20 text-white">Aguas Calientes</p>
+                    </div>
+
+                    <div className="flexBetween">
+                        <div className="flex flex-col">
+                            <p className="regular-16 block text-gray-20">
+                                Distance
+                            </p>
+                            <p className="bold-20 text-white">173.28 mi</p>
+                        </div>
+                        <div className="flex flex-col">
+                            <p className="regular-16 block text-gray-20">
+                                Elevation
+                            </p>
+                            <p className="bold-20 text-white">2.040 km</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </section>
     );
 }
