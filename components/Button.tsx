10efa91b6cf5812b0c2446ec42b2f7033a60650e
HoMaiQue <homaique.it@gmail.com>
2023-10-22 12:40:53 +0700

    feature, getapp and footer section

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,16 +6,19 @@ interface Props {
     title: string;
     icon?: string;
     variant: string;
+    full?: boolean;
 }
 
-const Button: React.FC<Props> = ({ type, title, icon, variant }) => {
+const Button: React.FC<Props> = ({ type, title, icon, variant, full }) => {
     return (
         <button
             type={type}
-            className={`flexCenter gap-3 rounded-full border ${variant}`}
+            className={`flexCenter gap-3 rounded-full border ${variant} ${
+                full && "w-full"
+            }`}
         >
             {icon && <Image src={icon} alt={title} height={24} width={24} />}
-            <label htmlFor="" className="whitespace-nowrap font-bold">
+            <label htmlFor="" className="whitespace-nowrap font-bold cursor-pointer">
                 {title}
             </label>
         </button>
